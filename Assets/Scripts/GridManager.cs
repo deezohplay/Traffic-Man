04a0b9976fb32651dc35ad3a8dbfd880e300f8cb
Rubangakene Inno Joseph <57228148+deezohplay@users.noreply.github.com>
2024-09-12 09:53:59 +0300

    Update GridManager.cs

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int rows = 6;
+    public int columns = 6;
+    public float tileSize = 1.0f;
+
     void Start()
     {
-        
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateGrid()
     {
-        
+        GameObject gridTile = new GameObject();
+        gridTile.transform.position = new Vector3(0, 0, 0);
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                GameObject tile = Instantiate(gridTile, transform);
+                float posX = col * tileSize;
+                float posY = row * tileSize;
+                tile.transform.position = new Vector3(posX, posY, 0);
+                tile.name = "Tile_" + row + "_" + col;
+            }
+        }
+        Destroy(gridTile); // Remove the template object
     }
 }
